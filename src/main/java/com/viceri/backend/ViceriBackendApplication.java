3cf6e71d980c0e5d2516e7222df8ef5044d0c559
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-02-21 08:06:41 -0300

    projeto criado

---
+++ src/main/java/com/viceri/backend/ViceriBackendApplication.java
@@ -0,0 +1,13 @@
+package com.viceri.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViceriBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViceriBackendApplication.class, args);
+	}
+
+}
